0ef57ba51da32f1ef23e644ad0aa6375faec2807
Victor de Souza Solique <victordesouzasolique@gmail.com>
2024-03-22 13:17:09 -0400

    commit 22/03

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,22 @@
+const url = "https://back-end-ifms-five.vercel.app/campi";
 
 export default async function Home() {
 
+  const resposta = await fetch(url,{
+    next: {
+      revalidate: 1
+    }
+  });
+  const campi = await resposta.json();
+ 
   return (
     <main className="text-center py-5 mb-5">
-      <h3>&lt;--  HOME  --&gt;</h3>
-      <p>Victor de Souza Solique</p>
+      <h3 className="pb-3">&lt;--  Página Home  --&gt;</h3>
+      {campi.map((campus) =>
+        <div>
+             <p>{campus.nome_campus}</p>
+        </div>
+      )}
     </main>
   )
 }
